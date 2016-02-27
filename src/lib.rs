f3b5943a0201ba37828f08da2ac9f8152f729abb
Felix S. Klock II <pnkfelix@pnkfx.org>
2016-02-27 02:44:24 +0100

    demo diamond import.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
+extern crate add6;
+extern crate add6_64;
+
 #[cfg(test)]
 mod tests {
+    use add6;
+    use add6_64;
+
     #[test]
     fn it_works() {
+        assert_eq!(add6::add6(10), 16_i32);
+        assert_eq!(add6_64::add6(10), 16_i64);
     }
 }
